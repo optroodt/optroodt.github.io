bee87fba02157cc3962191d98897a554dd3c285e
Youri op 't Roodt <y.optroodt@gmail.com>
2026-01-27 22:28:09 +0100

    Change baseurl to /blog

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,114 +2,114 @@ var store = [{
         "title": "Hacking an old 36-78mm Canon lens into a macro lens",
         "excerpt":"Introduction Hacking an old 36-78mm Canon lens into a macro lens Recently my old analog EOS 300 camera’s shutter stopped working. I still had a bunch of films I wanted to shoot so I thought I’d buy a couple of these camera’s on Marktplaats. I found that they were really...","categories": [],
         "tags": ["canon","macro","lens","hack"],
-        "url": "/hacking-an-old-36-78mm-canon-lens-into-a-macro-lens/",
+        "url": "/blog/hacking-an-old-36-78mm-canon-lens-into-a-macro-lens/",
         "teaser": null
       },{
         "title": "Upgrading 1st gen AppleTV to play Full HD",
         "excerpt":"This morning I installed the Broadcom CrystalHD BCM70012 Mini PCIe card into my AppleTV. I had to take out the WiFi card that was in the slot, but as my home is fully wired I couldn’t care less. The card is a hardware decoder that can process AVC, VC-1 and MPEG...","categories": [],
         "tags": ["appletv","crystalhd","fullhd","xbmc"],
-        "url": "/upgrading-1st-gen-appletv-to-play-full-hd/",
+        "url": "/blog/upgrading-1st-gen-appletv-to-play-full-hd/",
         "teaser": null
       },{
         "title": "Replacing the lens module of a PlayStation 3",
         "excerpt":"Recently I was given a PlayStation 3 by a friend. Not a slim one, but the old school PS3 phat. He bought a new one because the one I was given wouldn’t read any discs anymore. It wouldn’t even to spin the disc. This actually makes sense: if the lens...","categories": [],
         "tags": ["ps3","lens","repair","bluray","ifixit"],
-        "url": "/replacing-the-lens-module-of-a-playstation-3/",
+        "url": "/blog/replacing-the-lens-module-of-a-playstation-3/",
         "teaser": null
       },{
         "title": "Save a laptop webcam",
         "excerpt":"Some time ago I received a broken laptop (HP G62 model A18ED). When I switched it on, the power LED and fans would go on, but that’s it. I hooked up an external screen to see if anything would happen but it did not. It was really dead so I...","categories": [],
         "tags": ["raspberrypi","webcam","lm317t","reuse","motion"],
-        "url": "/save-a-laptop-webcam/",
+        "url": "/blog/save-a-laptop-webcam/",
         "teaser": null
       },{
         "title": "Replacing the screen of an iPhone 4",
         "excerpt":"A year and a half ago my girlfriend dropped her phone, of course I would fix it. I’m not going to write my experience here, just follow iFixit’s guide, that’s what I did.   But I do have some pictures.       Keep track of what you take out!        ","categories": [],
         "tags": ["iphone4"],
-        "url": "/replacing-the-screen-of-an-iphone-4/",
+        "url": "/blog/replacing-the-screen-of-an-iphone-4/",
         "teaser": null
       },{
         "title": "Upgrading an iPod Mini to 64Gb",
         "excerpt":"I love the design of the old iPods, especially the 3rd generation, but unfortunately it doesn’t charge over USB. I bought a second hand 1st generation iPod mini (4Gb) online for about $20. In order to make it cool it had to be upgraded, right? It uses a Microdrive for...","categories": [],
         "tags": ["ipod","mini","sdxc","tarkan","cfadapter","ipod64gb"],
-        "url": "/upgrading-an-ipod-mini-to-64gb/",
+        "url": "/blog/upgrading-an-ipod-mini-to-64gb/",
         "teaser": null
       },{
         "title": "Repairing a broken Nintendo DS Lite hinge",
         "excerpt":"Recently I bought a few Nintendo DS Lites, just to tinker with them. I didn’t want to pay too much so I didn’t mind if something was wrong with them. I bought three from different sellers for about 50 euros (excl. postage) in total: one has a cart slot that...","categories": [],
         "tags": ["nds","nintendo","repair","hinge","teardown"],
-        "url": "/repairing-a-broken-nintendo-ds-lite-hinge/",
+        "url": "/blog/repairing-a-broken-nintendo-ds-lite-hinge/",
         "teaser": null
       },{
         "title": "Refurbishing a Game Boy DMG",
         "excerpt":"After my Nintendo DS adventure I decided to go a bit more retro and look for an original Game Boy, also referred to as the Game Boy DMG. In case you’re wondering, DMG stands for Dot Matrix Game Boy. As a kid I never had a Game Boy. I had...","categories": [],
         "tags": ["gameboy","nintendo","refurbish","deadpixel","deadcolumn","dmg"],
-        "url": "/refurbishing-a-game-boy-dmg/",
+        "url": "/blog/refurbishing-a-game-boy-dmg/",
         "teaser": null
       },{
         "title": "Game Boy backlight mod",
         "excerpt":"In my last post I refurbished a Game Boy DMG and a Pocket, but I still had the backlight kit from Gameboymods.co.uk. The backlight mod can be tricky because you have to remove the reflective film and polarizer from the LCD screen, which are firmly attached with adhesive. I read...","categories": [],
         "tags": ["gameboy","backlight","retrogaming","modding"],
-        "url": "/game-boy-backlight-mod/",
+        "url": "/blog/game-boy-backlight-mod/",
         "teaser": null
       },{
         "title": "What's inside Game Boy battery packs?",
         "excerpt":"With all the Game Boys I collected I sometimes received other Game Boy accessories, like a magnifying glass and a four player adapter. Most of all I got battery packs: the Nuby Snap ‘n’ Power™ and Naki™ Action-Pak+. I tried charging them but they would hardly hold any charge so...","categories": [],
         "tags": ["gameboy","nuby","snapnpower","nintendo","battery","naki"],
-        "url": "/what's-inside-game-boy-battery-packs/",
+        "url": "/blog/what's-inside-game-boy-battery-packs/",
         "teaser": null
       },{
         "title": "Powering a Game Boy over USB",
         "excerpt":"One of the cool things about the Game Boy is that it uses regular AA type batteries, which are ubiquitous. However, you might not always have (charged) AA batteries lying around. Nowadays you’re more likely to have a USB outlet somewhere around you. Since the Game Boy operates over quite...","categories": [],
         "tags": ["gameboy","dmg","usb"],
-        "url": "/powering-a-game-boy-over-usb/",
+        "url": "/blog/powering-a-game-boy-over-usb/",
         "teaser": null
       },{
         "title": "A Game Boy Backlight + Bivert mod",
         "excerpt":"After backlighting a Game Boy I was really wondering how big of a difference a bivert mod would make. In pictures you see on the internet the difference is quite dramatic, but I already thought that just a backlight looked amazing… I couldn’t be more wrong. First I installed a...","categories": [],
         "tags": ["gameboy","bivert","mod","backlight","dmg","handheldlegend","deadpanrobot"],
-        "url": "/a-game-boy-backlight-plus-bivert-mod/",
+        "url": "/blog/a-game-boy-backlight-plus-bivert-mod/",
         "teaser": null
       },{
         "title": "A Game Boy orange backlight and bivert mod",
         "excerpt":"Last time I ordered backlights from deadpanrobot, I also got an orange (or amber) one because I thought it would look cool and give a nice retro feel to an already retro console. Taking off the reflective film was very easy, I think I’ve really got the hang of it...","categories": [],
         "tags": ["gameboy","modding","backlight","retrogaming","retro","bivert"],
-        "url": "/a-game-boy-orange-backlight-and-bivert-mod/",
+        "url": "/blog/a-game-boy-orange-backlight-and-bivert-mod/",
         "teaser": null
       },{
         "title": "Pirate Game Boy Advance carts?",
         "excerpt":"I love the Nintendo DS Lite for its form factor and it’s versatility. It plays DS games and GBA games and with an R4 or M3 card it can even run emulators for NES or GB/GBC. I didn’t have any GBA games I could play so I wanted to get...","categories": [],
         "tags": ["gba","pirate","cartridges","nintendo","zelda"],
-        "url": "/pirate-game-boy-advance-carts/",
+        "url": "/blog/pirate-game-boy-advance-carts/",
         "teaser": null
       },{
         "title": "Flashing Game Boy cartridges",
         "excerpt":"I’ve been collecting some Game Boys and games over the past year and every now and then I see a game that I’d like to have but the prices on some retro games are hard to justify. Sure, you can download a rom and play them in an emulator, but...","categories": [],
         "tags": ["joeyjoebags","game boy","flashcart","bennvenn"],
-        "url": "/flashing-game-boy-cartridges/",
+        "url": "/blog/flashing-game-boy-cartridges/",
         "teaser": null
       },{
         "title": "Game Boy Advance backlight mod",
         "excerpt":"It’s been quiet for a while, busy playing Breath of the Wild and Mario Kart 8 Deluxe on the Nintendo Switch. I still follow what’s going on in the modding scene, I watch other people’s mods and occasionally pick up some cheap handhelds. A couple of things led me to...","categories": [],
         "tags": ["gameboy","gba","backlight","modding","handheld","console","ags101"],
-        "url": "/game-boy-advance-backlight-mod/",
+        "url": "/blog/game-boy-advance-backlight-mod/",
         "teaser": null
       },{
         "title": "Reflashing multi carts",
         "excerpt":"Previously I had only reflashed single game cartridges, but it’s also possible to flash multi carts! They are being sold as 22-in-1 Pokemon carts, but the names might vary. I found that these carts need a small modification in order to work. If you don’t perform this modification, it will...","categories": [],
         "tags": ["gameboy","joeyjoebags","bennvenn","multicart","pokemon","dmg"],
-        "url": "/reflashing-multi-carts/",
+        "url": "/blog/reflashing-multi-carts/",
         "teaser": null
       },{
         "title": "Replacing the battery on a Game Boy cartridge",
         "excerpt":"I hang out in /r/Gameboy/ a lot and I see that quite a few people mention J4U or japan_4_you on eBay as their resource for buying Game Boy related stuff. They have a lot of good stuff and some things are just dirt cheap! While browsing I found Japanese releases...","categories": [],
         "tags": ["gameboy","battery","japan4you"],
-        "url": "/replacing-the-battery-on-a-game-boy-cartridge/",
+        "url": "/blog/replacing-the-battery-on-a-game-boy-cartridge/",
         "teaser": null
       },{
         "title": "Backlighting a Game Boy Pocket with a burnt polarizer",
         "excerpt":"After the good experience I had with the Japanese Game Boy games from Japan 4 You I decided to pickup some cheap Game Boy Pockets. They list a lot of items as ‘not working’ or ‘read item condition’ for a low price. I’m not really interested in buying perfectly fine...","categories": [],
         "tags": ["gbp","backlight","japan4you","gameboypocket"],
-        "url": "/backlighting-a-game-boy-pocket-with-a-burnt-polarizer/",
+        "url": "/blog/backlighting-a-game-boy-pocket-with-a-burnt-polarizer/",
         "teaser": null
       }]
